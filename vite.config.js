40a6b7e100826f60ad31006c74b3a479fc9632e4
Yash Gorasia <y.t.gorasia@gmail.com>
2023-03-15 12:26:05 +0530

    Changed root folder to src

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
+
 export default defineConfig({
+  root,
   base: "./",
+  build: {
+    outDir,
+    emptyOutDir: true,
+    input: {
+      home: resolve(root, "index.html"),
+    },
+  },
 });
