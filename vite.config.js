9567cedc6806cab67cba8dd4dc6fb0f61f922999
Yash Gorasia <y.t.gorasia@gmail.com>
2023-03-17 17:53:42 +0530

    Blog 3 added

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
         home: resolve(root, "index.html"),
         what_is_bvoc: resolve(root, "what_is_bvoc.html"),
         bvoc_it: resolve(root, "bvoc_it.html"),
+        bvoc_hc: resolve(root, "bvoc_hc.html"),
       },
     },
   },
